63e26f2e79ef373c6beaf23eb438b005e69da3ca
Lamia Fatiha <f6lamia@gmail.com>
2024-06-23 14:16:52 +0600

    added weather api

--- src/components/Temp.js
+++ src/components/Temp.js
@@ -1,32 +1,56 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './css/style.css';
-
 const Temp = () => {
+
+    const [city,setCity] = useState(null);
+    const [search,setSearch] = useState();
+    useEffect(()=>{
+        const fetchApi = async () =>{
+            //const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&&appid={dfde7a48f4ef6018acda195877585eca}`;
+            const url = `http://api.weatherapi.com/v1/current.json?key= 2a4fde19cf754502b2880509242306&q=${search}`;
+
+            const response = await fetch(url);
+            const resJson = await response.json();
+         setCity(resJson.current);
+        };
+        fetchApi();
+    },[search])
     return (
         <>
         <div className="box">
           <div className="inputData">
              <input type="search" 
+             value={search}
              className="inputField"
              onChange={(event)=>{
-
+                  setSearch(event.target.value)
              }} />
           </div>
-        <div className="info">
+          {!city ? (
+            <p>Data not found</p>
+          ) : (
+            <div>
+            <div className="info">
             <h2 className='location'>
-            <i class="fa-solid fa-street-view"></i>
-            Barishal  
+            <i className="fa-solid fa-street-view"></i>
+            {search}  
             </h2>
             <h1 className="temp">
-
+               {city.temp_c}°
             </h1>
             <h3 className='temp-max'>
-            min: 13° | max : 13°
+            Min: {city.feelslike_c}° | Max: {city.humidity}°
             </h3>
         </div>
         <div className="wave -one"></div>
         <div className="wave -two"></div>
         <div className="wave -three"></div>
+            </div>
+
+          )
+
+          }
+
 </div>
         </>
     )
